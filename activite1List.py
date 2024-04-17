99b18b9e61014dd7a168822c91bb34cc8e65a4c4
unknown <imenhzl14@gmail.com>
2024-04-17 15:40:44 +0200

    � NEW:  activite1 pour chapitre list

---
+++ activite1List.py
@@ -0,0 +1,14 @@
+def chainelist(list):
+    #creation d'une nouvelle list
+    listFinal=[]
+    #parcourir la liste initiale 
+    for elt in list:
+        #si l'element de la liste initial n'existe pas dans la nouvelle list 
+        if elt not in listFinal:
+        #On ajoute l'element dans la nouvelle liste
+            listFinal.append(elt)
+    return listFinal
+
+list=["A","B","B",1,2,3,2]
+result=chainelist(list)
+print(result)
