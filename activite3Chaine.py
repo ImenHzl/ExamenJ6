ac2314fce93fd6c39a1c6d8ce976dd0431ce5a56
EL-Yasmina <yasminataif99@gmail.com>
2024-04-18 02:11:20 +0200

    👌 IMPROVE: Modifier et Ajouter des commantaires pour chaque ligne de code pour l'activite de chaine

--- activite3Chaine.py
+++ activite3Chaine.py
@@ -5,8 +5,8 @@ nombre1_str = input("Entrez le premier nombre: ")
 nombre2_str = input("Entrez le deuxième nombre: ")
 
 # Convertir les chaînes de caractères en nombres entiers
-try:
-  nombre1 = int(nombre1_str)
+try:    #try except pour la gestion d'erreur si l'utilisateur ne tape pas un chiffre j'affiche un message erreur.
+  nombre1 = int(nombre1_str)   #j'ai utilisé la fonction int pour convertir la chaine de caract en nombre entiers.
   nombre2 = int(nombre2_str)
 except ValueError:
   print("Erreur: Veuillez saisir uniquement des nombres entiers.")
@@ -16,4 +16,4 @@ except ValueError:
 somme = nombre1 + nombre2
 
 # Afficher le résultat
-print(f"La somme de {nombre1} et {nombre2} est : {somme}")
+print(f"La somme de {nombre1} et {nombre2} est : {somme}")  
