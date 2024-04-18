65ad7b3cda3b582dbf100216c685b89dfa4199e9
unknown <imenhzl14@gmail.com>
2024-04-18 15:36:05 +0200

    � IMPROVE: activite 1 boucle ajout bravo

--- activite1Boucle.py
+++ activite1Boucle.py
@@ -2,13 +2,13 @@
 import random
 
 
-def devineNombre():
+def devineNombre(limite):
     # choisir un nombre aléatoire de 1 à 100
     nombre_aleatoire = random.randint(1, 100)
     # afficher le nombre choisi
     print(nombre_aleatoire)
     # pour limiter le nombre de tentative
-    limite = 5
+    
     # initialiser le tentative à 1
     i = 1
     # refaire le test jusqu'a 5
@@ -16,13 +16,15 @@ def devineNombre():
         # saisir le nombre par clavier
         nombre_alt = input("Devine le nombre:")
         # si le nombre saisi est plus petit que le nombre choisi aléatoirement il affiche un message plus petit
-        if int(nombre_alt) >= int(nombre_aleatoire):
+        if int(nombre_alt) > int(nombre_aleatoire):
             print("Le nombre est plus petit")
         # sinon il affiche le nombre est plus grand
-        else:
+        elif int(nombre_alt) < int(nombre_aleatoire):
             print("le nombre est plus grand")
+        else:
+            print("BRAVOO")
         # incrementer la tentative
         i += 1
 
 
-result = devineNombre()
+result = devineNombre(5)
