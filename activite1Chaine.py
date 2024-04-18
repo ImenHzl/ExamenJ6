4f7f36e0f7a6464ea6adc666ba1c528de8cb860b
unknown <imenhzl14@gmail.com>
2024-04-18 11:33:18 +0200

    👌 IMPROVE: activite verifier avec flake8

--- activite1Chaine.py
+++ activite1Chaine.py
@@ -1,16 +1,18 @@
-def chaineCar(chain):   
+def chaineCar(chain):
     nbrMaj = 0
     nbrMin = 0
-    #boucle sur chaque caractere de la chaine qu'on a saisi
+    # boucle sur chaque caractere de la chaine qu'on a saisi
     for i in chain:
-        #si le caractere est majuscule on va incrementer nbrMaj
+        # si le caractere est majuscule on va incrementer nbrMaj
         if i.isupper():
             nbrMaj += 1
-        #si le caractere est miniscule on va incrementer nbrMin
+        # si le caractere est miniscule on va incrementer nbrMin
         else:
             nbrMin += 1
-    return nbrMaj,nbrMin
+    return nbrMaj, nbrMin
+
+
 chaine = input("entrez une chaine de caractère: ")
-maj,min = chaineCar(chaine)
+maj, min = chaineCar(chaine)
 print(f"nombre Majuscule:{maj}")
 print(f"nombre Miniscule:{min}")
